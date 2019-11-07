e594d8ef69464f3a249a4b1e9a901d05df80e2a1
Adrian Vinatoru <adrian.vinatoru@tradeshift.com>
2019-11-07 09:36:15 +0200

    first commit

---
+++ src/main/java/com/kreator/Favorites/Albums/FavoritesAlbumsApplication.java
@@ -0,0 +1,13 @@
+package com.kreator.Favorites.Albums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoritesAlbumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoritesAlbumsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kreator/Favorites/Albums/FavoritesAlbumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kreator.Favorites.Albums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoritesAlbumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
